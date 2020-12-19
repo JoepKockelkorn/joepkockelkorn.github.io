53de4ea8f5a5f81aea9e7e0851857645fb596fa8
Joep Kockelkorn <joepkockelkorn@gmail.com>
2020-12-19 23:17:08 +0100

    build: add scully

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
