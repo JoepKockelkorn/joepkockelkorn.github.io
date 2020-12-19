87170f89fdbd1413cb02a7fbe104195d95aa5ff8
Joep Kockelkorn <joepkockelkorn@gmail.com>
2020-12-19 23:21:46 +0100

    build: add angular, nx and scully and publish outputs (#1)

---
+++ scully.app.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./apps/app/src",
+  projectName: "app",
+  outDir: './dist/static',
+  routes: {
+  }
+};
