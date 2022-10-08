6b488e4674248053f3bfb55c11397464e729a7b8
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-10-08 14:27:29 +0200

    chore: remove old website and redirect to new one (#4)

--- scully.app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ScullyConfig } from '@scullyio/scully';
-export const config: ScullyConfig = {
-  projectRoot: "./apps/app/src",
-  projectName: "app",
-  outDir: './dist/static',
-  routes: {
-  }
-};
